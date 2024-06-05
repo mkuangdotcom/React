fb949f504d202e2c25c30073057ed37e886f3cc8
Foo Ming Kuang <150900178+mkuangdotcom@users.noreply.github.com>
2024-06-05 15:51:09 +0800

    export

--- api/weather.js
+++ api/weather.js
@@ -1,2 +1,20 @@
 const apiKey = '4dfeaf4809f242c58a073811240506';
-const apiURL = params =>``;
+const apiURL = params =>`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.country}&days=${params.days}&aqi=no&alerts=no`;
+
+const apiCall = async (URL) => {
+    const options = {
+        method: 'Get',
+        url: URL
+    }
+    try {
+        const response = await axios.request(options);
+        return response.data;
+    } catch(err) {
+        console.log('error',err);
+        return null;
+    }
+}
+
+export const fetchWeatherForecast = params => {
+    return apiCall(apiURL(params));
+}
